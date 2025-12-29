ea731ff47fd6bcc828490c1045d498b049dfcba3
MH Emon <mhemon@MHs-MacBook-Air.local>
2025-12-29 12:24:50 +0600

    React Router setup

--- src/layouts/MainLayouts.jsx
+++ src/layouts/MainLayouts.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Header from '../components/home/Header';
+import { Outlet } from 'react-router';
+
 
 const MainLayouts = () => {
     return (
--- src/main.jsx
+++ src/main.jsx
@@ -1,40 +1,37 @@
-import { Children, Component, StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { createBrowserRouter } from "react-router";
-import { RouterProvider } from "react-router/dom";
-import MainLayouts from './layouts/MainLayouts.jsx';
-import Home from './components/home/Home.jsx';
-import AddCoffee from './components/addCoffee/AddCoffee.jsx';
-import UpdateCoffee from './components/updatecoffee/UpdateCoffee.jsx';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import MainLayouts from "./layouts/MainLayouts.jsx";
+import Home from "./components/home/Home.jsx";
+import AddCoffee from "./components/addCoffee/AddCoffee.jsx";
+import UpdateCoffee from "./components/updateCoffee/UpdateCoffee.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    Component: MainLayouts,
-    Children: [
+    element: <MainLayouts />,
+    children: [
       {
         index: true,
-        Component: Home
+        element: <Home />,
       },
       {
-        path: 'addCoffee',
-        Component: AddCoffee
+        path: "addCoffee",
+        element: <AddCoffee />,
       },
       {
-        path: 'updateCoffee',
-        Component: UpdateCoffee
-      }
-    ]
+        path: "updateCoffee",
+        element: <UpdateCoffee />,
+      },
+    ],
   },
 ]);
 
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />,
-
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
