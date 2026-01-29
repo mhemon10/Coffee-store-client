78dced5ad7290f0b5759cd28c14ffb25ce7344be
MH Emon <mhemon833@gmail.com>
2026-01-29 12:22:55 +0600

    Home page hero section done

---
+++ src/components/home/HeroSection.jsx
@@ -0,0 +1,37 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import MainLayouts from "./layouts/MainLayouts.jsx";
+import Home from "./components/home/Home.jsx";
+import AddCoffee from "./components/addCoffee/AddCoffee.jsx";
+import UpdateCoffee from "./components/updateCoffee/UpdateCoffee.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayouts />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "addCoffee",
+        element: <AddCoffee />,
+      },
+      {
+        path: "updateCoffee",
+        element: <UpdateCoffee />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+);
